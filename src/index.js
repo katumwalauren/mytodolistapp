84105ef255b40d7ae139ec42ea66dc38097444f3
katumwalauren <95852478+katumwalauren@users.noreply.github.com>
2024-03-15 22:49:27 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./styles/GlobalStyles.css";
+import '@fontsource/poppins';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+
+ ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
